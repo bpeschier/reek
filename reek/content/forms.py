3c06a37b1cdd62c328698c1aefee11399f74ef83
Bas Peschier <bpeschier@fizzgig.nl>
2014-04-13 12:50:31 +0200

    Start working on the editor

--- reek/content/forms.py
+++ reek/content/forms.py
@@ -1,22 +1,27 @@
 from django import forms
 from django.template.loader import render_to_string
+from django.utils.encoding import force_text
 
 
 class ContentWidget(forms.Widget):
     choices = []
 
     def render(self, name, value, attrs=None):
-        context = {
-            'name': name,
-            'value': value,
-        }
-        context.update(attrs)
-        return render_to_string('reek/content/widgets/contentfield.html', context)
+        if value is None:
+            value = ''
+        final_attrs = self.build_attrs(attrs, name=name)
+        if value != '':
+            # Only add the 'value' attribute if a value is non-empty.
+            final_attrs['value'] = force_text(self._format_value(value))
+        return render_to_string('reek/content/widgets/contentfield.html', final_attrs)
 
     @property
     def media(self):
         return forms.Media(
-            js=('js/content-editor.js',)
+            js=('content/js/content-editor.js',),
+            css={
+                'screen': ('content/css/content-editor.css',),
+            },
         )
 
 
